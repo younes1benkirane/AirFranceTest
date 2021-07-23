a71fa8c18ee4c2ac5d51a5c38ab8181dec707a99
ybenkirane <younes.benkirane@aymax.fr>
2021-07-23 16:28:05 +0200

    add user entity

---
+++ src/main/java/com/airfrance/AirFranceTest/entity/User.java
@@ -0,0 +1,156 @@
+package com.airfrance.AirFranceTest.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
+
+/**
+ * <p>
+ *     class User Entity
+ * </p>
+ * @author Younes Benkirane
+ */
+@Entity
+@Table(name="user")
+public class User {
+    private String id;
+
+    private String login;
+
+    private String fullName;
+
+    private String birthday;
+
+    private String country;
+
+    private String gender;
+
+    private Long phoneNumber;
+
+    public User() {
+    }
+
+    /**
+     * <p>
+     *     Constructor with parameters
+     * </p>
+     * @param id
+     * @param login
+     * @param fullName
+     * @param birthday
+     * @param country
+     *
+     */
+    public User(String id, String login, String fullName, String birthday, String country) {
+        this.id = id;
+        this.login = login;
+        this.fullName = fullName;
+        this.birthday = birthday;
+        this.country = country;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    @NotEmpty
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * <p>
+     *     User equals method
+     * </p>
+     * @param o
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(login, user.login) && Objects.equals(fullName, user.fullName) && Objects.equals(birthday, user.birthday) && Objects.equals(country, user.country) && Objects.equals(gender, user.gender) && Objects.equals(phoneNumber, user.phoneNumber);
+    }
+
+    /**
+     * <p>
+     *     HashCode  method
+     * </p>
+     * @return int
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, fullName, birthday, country, gender, phoneNumber);
+    }
+
+    /**
+     * <p>
+     *     ToString method
+     * </p>
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", login='" + login + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", birthday='" + birthday + '\'' +
+                ", country='" + country + '\'' +
+                ", gender='" + gender + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
