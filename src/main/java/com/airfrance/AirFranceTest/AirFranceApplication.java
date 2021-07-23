5045a11c2404f8688428ccf95423653af687fd63
ybenkirane <younes.benkirane@aymax.fr>
2021-07-23 16:00:20 +0200

    spring boot run application

--- src/main/java/com/airfrance/AirFranceTest/AirFranceApplication.java
+++ src/main/java/com/airfrance/AirFranceTest/AirFranceApplication.java
@@ -1,2 +1,9 @@
-package com.airfrance.AirFranceTest;public class AirFranceApplication {
+package com.airfrance.AirFranceTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AirFranceApplication {
+   public static void main(String [] args) {
+      SpringApplication.run(AirFranceApplication.class, args);
+    }
 }
