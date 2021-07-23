c31a33c62cdb2a9eca7d352603b3f1375e9321c2
ybenkirane <younes.benkirane@aymax.fr>
2021-07-23 16:37:24 +0200

    add user dto

---
+++ src/main/java/com/airfrance/AirFranceTest/dto/UserDto.java
@@ -0,0 +1,71 @@
+package com.airfrance.AirFranceTest.dto;
+
+/**
+ * <p>
+ *     class User Dto
+ * </p>
+ *
+ * @author Younes Benkirane
+ */
+public class UserDto {
+
+    private String login;
+
+    private String fullName;
+
+    private String birthday;
+
+    private String country;
+
+    private String gender;
+
+    private Long phoneNumber;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
